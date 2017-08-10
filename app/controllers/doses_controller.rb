c13a235cf29ce8613b165e3abcb6ba2fdec94173
Vincent Salamand <salamandvincent@gmail.com>
2017-08-10 17:07:24 +0200

    add user stories

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,23 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
